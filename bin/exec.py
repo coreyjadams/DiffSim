Add requirements; use 1/r^2 scaling for sipm response, could be incorrect

bin/exec.py
@@ -21,6 +21,7 @@ from hydra.core.config_store import ConfigStore
 
 hydra.output_subdir = None
 
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
@@ -28,8 +29,11 @@ from jax import random
 import jax.numpy as numpy
 import jax.tree_util as tree_util
 
+# from jax.config import config
+# config.update('jax_disable_jit', True)
 
 
+from tensorboardX import SummaryWriter
 
 
 # Add the local folder to the import path:
@@ -40,8 +44,8 @@ sys.path.insert(0,src_dir)
 from config import Config
 from config import MPI_AVAILABLE, NAME
 
-if MPI_AVAILABLE:
-    import horovod.tensorflow as hvd
+# if MPI_AVAILABLE:
+#     import horovod.tensorflow as hvd
 
 
 
@@ -122,9 +126,9 @@ class exec(object):
 
 
         # TODO: network snapshots
-        # if not MPI_AVAILABLE or hvd.rank() == 0:
-        #     # self.writer = tf.summary.create_file_writer(self.save_path)
-        #     self.writer = tf.summary.create_file_writer(self.save_path + "/log/")
+        if not MPI_AVAILABLE or hvd.rank() == 0:
+            # self.writer = tf.summary.create_file_writer(self.save_path)
+            self.writer = SummaryWriter(self.save_path + "/log/")
 
         # self.build_hp(self.writer)
 
@@ -163,12 +167,10 @@ class exec(object):
     #       )
     #     hp.hparams(hparams)  # record the values used in this trial
     #     accuracy = train_test_model(hparams)
-    #     tf.summary.scalar(METRIC_ACCURACY, accuracy, step=1)
+    #     tf.summary.scalar(METRIC_ACCURACY, accuracy, 1)
 
 
     def configure_logger(self):
-
-        print("Configuring logger")
 
         logger = logging.getLogger(NAME)
 
@@ -213,10 +215,10 @@ class exec(object):
         return dl
 
     def build_simulator(self, batch, subkey):
-        from simulator.NEW_Simulator import simulate_pmts, init_params
+        from simulator.NEW_Simulator import simulate_waveforms, init_params
 
         # return the function but return initialized params:
-        return simulate_pmts, init_params(subkey, batch)
+        return simulate_waveforms, init_params(subkey, batch)
 
     def build_trainer(self, batch, fn, params):
 
@@ -349,7 +351,7 @@ class exec(object):
             # metrics.update(simulator_metrics)
 
 
-            # self.summary(metrics, self.global_step)
+            self.summary(metrics, self.global_step)
 
             # Add comparison plots every iteration for now:
             if self.global_step % self.config.run.image_iteration == 0:
@@ -357,13 +359,11 @@ class exec(object):
                     save_dir = self.save_path / pathlib.Path(f'comp/{self.global_step}/')
                     self.trainer.comparison_plots(save_dir)
 
-            # # Add the gradients and model weights to the summary every 25 iterations:
-            # if self.global_step % 25 == 0:
-            #     if not MPI_AVAILABLE or hvd.rank() == 0:
-            #         weights = self.sr_worker.wavefunction.trainable_variables
-            #         gradients = self.sr_worker.latest_gradients
-            #         self.model_summary(weights, gradients, self.global_step)
-            #         self.wavefunction_summary(self.sr_worker.latest_psi, self.global_step)
+            # Add the weights to the summary every 100 iterations:
+            if self.global_step % 100 == 0:
+                if not MPI_AVAILABLE or hvd.rank() == 0:
+                    parameters = self.trainer.parameters()
+                    self.model_summary(parameters, self.global_step)
 
 
             if self.global_step % 1 == 0:
@@ -438,23 +438,22 @@ class exec(object):
             self.global_step += 1
 
 
-    def model_summary(self, weights, gradients, step):
-        with self.writer.as_default():
-            for w, g in zip(weights, gradients):
-                tf.summary.histogram("weights/"   + w.name, w, step=step)
-                tf.summary.histogram("gradients/" + w.name, g, step=step)
+    def model_summary(self, weights, step):
+        # with self.writer.as_default():
+        for key in weights:
+            self.writer.add_histogram("weights/" + key, weights[key], step)
 
     def wavefunction_summary(self, latest_psi, step):
-        with self.writer.as_default():
-            tf.summary.histogram("psi", latest_psi, step=step)
+        # with self.writer.as_default():
+        self.writer.add_histogram("psi", latest_psi, step)
 
 
     # @tf.function
     def summary(self, metrics, step):
         if not MPI_AVAILABLE or hvd.rank() == 0:
-            with self.writer.as_default():
-                for key in metrics:
-                    tf.summary.scalar(key, metrics[key], step=step)
+            # with self.writer.as_default():
+            for key in metrics:
+                self.writer.add_scalar(key, metrics[key], step)
 
 
     def save_weights(self):

src/simulator/NEW_Simulator.py
@@ -6,13 +6,20 @@ from jax import random
 from jax.example_libraries import stax
 import jax.tree_util as tree_util # for applying learning rate to gradients
 
+# This is a global variable defining the sipm locations:
+sipms_1D = np.arange(-235, 235, 10.)
+n_sipms = sipms_1D.shape[0]
+sipm_locations_x = np.tile(sipms_1D, (n_sipms,)).reshape((n_sipms, n_sipms))
+sipm_locations_y = np.tile(sipms_1D, (n_sipms,)).reshape((n_sipms, n_sipms)).transpose()
+
+sipm_locations = np.stack([sipm_locations_y, sipm_locations_x], -1)
 
 # JAX Is pure functional programming.  So, we're gonna build up to one function here:
 
 
 # Build the whole thing as a function we can JIT:
 # @jit
-def simulate_pmts(energy_depositions, parameters, key):
+def simulate_waveforms(energy_depositions, parameters, key):
     # Split the key for generation:
     key, subkey = random.split(key)
     generated_electrons, valid_electrons = \
@@ -26,46 +33,78 @@ def simulate_pmts(energy_depositions, parameters, key):
     lifetime = compute_lifetime(diffused, parameters["lifetime"], valid_electrons)
 
     # Split off the XY and Z:
-    
+
     diffused_xy = diffused[:,:,:,0:2]
     diffused_z  = diffused[:,:,:,2:]
 
 
-    # Compute the PMT response:
+    # Compute the PMT response to each energy deposition:
     pmt_response = batch_pmt_nn_apply(parameters["pmt_network"], diffused_xy)**2
-
-
+    # print("pmt_response.shape: ", pmt_response.shape )
+    # Normalize the PMT response per PMT:
     pmt_response = pmt_response * (parameters["pmt_dynamic_range"])**2
 
     # Compute the waveforms:
-    pmt_waveforms = batch_build_waveforms(
+    pmt_waveforms = batch_build_pmt_waveforms(
         pmt_response, diffused_z, lifetime, 0.1)
         # pmt_response, diffused_z, lifetime, parameters["waveform_sigma"])
     # Sum over the individual depositions:
     pmt_waveforms = pmt_waveforms.sum(axis=1)
-    return pmt_waveforms
+
+    # print("pmt_waveforms.shape: ", pmt_waveforms.shape )
+
+    # Compute the response of the EL region for the SIPMs.
+    # It's an isotropic emission, assumed, so that means
+    # It should fall off as 1/r^2 for each SIPM.
+
+    # Square it to ensure positive:
+    sipm_response = batch_sipm_nn_apply(parameters["sipm_network"], diffused_xy)**2
+
+
+    # print("sipm_response.shape: ", sipm_response.shape )
+    sipm_waveforms = batch_build_sipm_waveforms(
+        sipm_response, diffused_xy, diffused_z, lifetime, 0.1)
+    sipm_waveforms = sipm_waveforms.sum(axis=1)
+
+
+    # TODO: RIght here!!!
+    # Need to reshape the sipm response
+    # print("sipm_waveforms.shape: ", sipm_waveforms.shape )
+
+    init_shape = sipm_waveforms.shape
+    # TODO: RIght here!!!
+    # sipm_waveforms = sipm_waveforms.reshape([init_shape[0], 47,47, init_shape[-1]])
+    # sipm_waveforms = sipm_waveforms * parameters["sipm_dynamic_range"]
+    # print("sipm_waveforms.shape: ", sipm_waveforms.shape )
+
+
+    return pmt_waveforms, sipm_waveforms
 
 # Another critical function is the parameter initialization:
 def init_params(key, example_input):
 
     # The input here needs to be a single electron deposition
     # but we splice that off here, to make it easier.
-    
+
     # Index into the batch dimension and the energy deposit in the batch:
     pmt_input, n_valid = generate_electrons(example_input["energy_deposits"][0][0], key)
     # Diffuse them:
     pmt_input = diffuse_electrons(pmt_input, np.ones(3), key)
 
-    pmt_input_shape = pmt_input[:,0:2].shape
-    
+    input_shape = pmt_input[:,0:2].shape
 
-    output_size, pmt_network_params = pmt_nn_init(key, pmt_input_shape)
+
+    pmt_output_size,  pmt_network_params  =  pmt_nn_init(key, input_shape)
+    sipm_output_size, sipm_network_params = sipm_nn_init(key, input_shape)
+
 
     parameters = {
         "diffusion"   : 0.1*np.ones(3),
         "lifetime"    : 5000*np.ones(1),
         "pmt_network" : pmt_network_params,
+        "sipm_network" : sipm_network_params,
         "pmt_dynamic_range" : np.ones(12),
+        "sipm_dynamic_range" : np.ones([1,47,47,1]),
         "waveform_sigma" : np.asarray(0.1)
     }
 
@@ -76,23 +115,23 @@ def init_params(key, example_input):
 def generate_electrons(energy_and_position, key):
     '''
     Generate a sequence of electrons given an energy amount.
-    A random number of electrons are generated, but a static-sized array 
+    A random number of electrons are generated, but a static-sized array
     is returned.  This ensures jit'ing is possible later.
     The number of electrons in the array that is valid is also returned
     '''
-    
+
     energy   = energy_and_position[-1]
     position = energy_and_position[0:3]
-    
+
     # For each energy, compute n:
     n = energy* 1000.*1000. / 22.4
     sigmas = np.sqrt(n * 0.15)
     # Generate a sample for each energy:
     n_electrons = (sigmas*random.normal(key) + n).astype(np.int32)
-    
+
     shape = (2000, 3)
     el_arr = np.broadcast_to(position, shape)
-    
+
     return el_arr, n_electrons
 
 # Vmap and jit these out to batches:
@@ -105,21 +144,21 @@ def diffuse_electrons(electrons, diffusion_scale, key):
     '''
     Apply diffusion to the electrons in a single energy deposition.
     '''
-    
+
     # Then input should be an array of shape [max_electrons, 3]
     # only some of these are valid, but that's ok, it's not
     # that wasteful to diffuse them all.
-    
+
     # Get the z position (we only need this as a scalar!):
     z = electrons[-1,-1]
     n_samples = electrons.shape[0]
-    
+
     # Sample from a normal distribution:
     kicks = random.normal(key, (electrons.shape))
-    
+
     # Scale the kicks by the diffusion scale and sqrt(z)
     return electrons + z*(diffusion_scale**2)*kicks
-    
+
 
 # Likewise, jit and vmap these out:
 diffuse_electrons_event = jit(vmap(diffuse_electrons, in_axes=[0,None, None]))
@@ -150,7 +189,7 @@ compute_probability = jit(vmap(e_compute_probability, in_axes=[0,None]))
 
 e_compute_mask = jit(vmap(s_compute_mask))
 compute_mask = jit(vmap(e_compute_mask))
-    
+
 @jit
 def compute_lifetime(electrons_batch, _lifetime, n_valid_batch):
     return compute_mask(electrons_batch, n_valid_batch) * compute_probability(electrons_batch, _lifetime)
@@ -168,42 +207,113 @@ pmt_nn_init, pmt_nn_apply = stax.serial(
 event_pmt_nn_apply = jit(vmap(jit(pmt_nn_apply), in_axes=[None, 0]))
 batch_pmt_nn_apply = jit(vmap(event_pmt_nn_apply, in_axes=[None, 0]))
 
+# Define the SiPM network and it's worker functions:
+sipm_nn_init, sipm_nn_apply = stax.serial(
+    stax.Dense(28), stax.Tanh,
+    stax.Dense(128), stax.Tanh,
+    #stax.Dense(512), stax.Tanh,
+    stax.Dense(1),
+)
+event_sipm_nn_apply = jit(vmap(jit(sipm_nn_apply), in_axes=[None, 0]))
+batch_sipm_nn_apply = jit(vmap(event_sipm_nn_apply, in_axes=[None, 0]))
+
 
 # Functions to build waveforms based on weights and responses:
 @jit
-def build_waveforms(_sensor_response, _z_positions, _weights, _bin_sigma):
+def build_pmt_waveforms(_sensor_response, _z_positions, _weights, _bin_sigma):
     '''
     Compute the PMT response to electrons on the EL region
     '''
     # This is basically a constant:
     _n_ticks=550
-    
+
     n_electrons = _z_positions.shape[0]
     # Build a range for the exponential input:
     starts = np.zeros(shape=(n_electrons)) # + 0.5
     stops  = np.ones(shape=(n_electrons)) * (_n_ticks -1) # + 0.5
-    
+
     # Reshape z positions for broadcasting:
     _z_positions = _z_positions.reshape((-1,1))
-    
+
 
     exp_input = np.linspace(start=starts, stop=stops, num=_n_ticks, axis=-1)
 
     exp_values = np.exp( - (exp_input - _z_positions)**2.  / (2. * _bin_sigma))
-    
+
     # Normalize the values:
     exp_values = exp_values.transpose() * (0.39894228040/np.sqrt(_bin_sigma))
-    
+
     # Scale by the weights:
     exp_values = exp_values * _weights
-    
+
+    # print("pmt exp_values.shape: ", exp_values.shape)
+    # print("pmt _sensor_response.shape: ", _sensor_response.shape)
     waveforms = np.matmul(exp_values, _sensor_response)
-    
+    # print("pmt waveforms.shape: ", waveforms.shape)
     return waveforms.transpose()
 
-e_build_waveforms = jit(vmap(build_waveforms, in_axes=[0,0,0,None,]))
-batch_build_waveforms = jit(vmap(e_build_waveforms, in_axes=[0,0,0,None,]))
+e_build_pmt_waveforms = jit(vmap(build_pmt_waveforms, in_axes=[0,0,0,None,]))
+batch_build_pmt_waveforms = jit(vmap(e_build_pmt_waveforms, in_axes=[0,0,0,None,]))
 
 
+# Functions to build waveforms based on weights and responses:
+@jit
+def build_sipm_waveforms(_sensor_response, _xy_positions, _z_positions, _weights, _bin_sigma):
+    '''
+    Compute the PMT response to electrons on the EL region
+    '''
+    # This is basically a constant:
+    _n_ticks=550
+
+    n_electrons = _z_positions.shape[0]
+    # Build a range for the exponential input:
+    starts = np.zeros(shape=(n_electrons)) # + 0.5
+    stops  = np.ones(shape=(n_electrons)) * (_n_ticks -1) # + 0.5
+
+    # Reshape z positions for broadcasting:
+    _z_positions = _z_positions.reshape((-1,1))
+
+    # print("_sensor_response: ", _sensor_response.shape)
+    # print("_xy_positions: ", _xy_positions.shape)
+    # print("_z_positions: ", _z_positions.shape)
+    # print("_weights: ", _weights.shape)
+    # print(sipm_locations.shape)
+    _xy_reshaped = _xy_positions.reshape((_xy_positions.shape[0], 1,1,_xy_positions.shape[-1]))
+    subtracted_values = _xy_reshaped - sipm_locations
+
+    r_squared = subtracted_values**2
+    r_squared = 1./(r_squared.sum(axis=-1) + 1.0) # This extra addition is the "standoff distance"
+    # print("r_squared.shape: ", r_squared.shape)
+    # print("_sensor_response.shape: ", _sensor_response.shape)
+
+    # Multiple the total light (sensor response) by the 1/r^2 scaling:
+    _sensor_response = _sensor_response.reshape((-1,1,1)) * r_squared
+
+    # We put the subtracted differences through a 1/r^2 response
+
+    exp_input = np.linspace(start=starts, stop=stops, num=_n_ticks, axis=-1)
+
+    exp_values = np.exp( - (exp_input - _z_positions)**2.  / (2. * _bin_sigma))
+
+    # Normalize the values:
+    exp_values = exp_values.transpose() * (0.39894228040/np.sqrt(_bin_sigma))
+
+    # Scale by the weights:
+    exp_values = exp_values * _weights
+
+    # print("exp_values.shape: ", exp_values.shape)
+
+    # print("sipm _sensor_response.shape: ", _sensor_response.shape)
+    # To do the matmul, we have to flatten the _sensor_response briefly
+    _sensor_response_flat = _sensor_response.reshape((-1, 47*47))
+    # print("sipm _sensor_response_flat.shape: ", _sensor_response_flat.shape)
+    # print("sipm exp_values.shape: ", exp_values.shape)
+    waveforms = np.matmul(exp_values, _sensor_response_flat)
+    # print("sipm waveforms.shape: ", waveforms.shape)
+    # And, unflatten:
+    waveforms = waveforms.reshape((-1, 47, 47))
+    return waveforms.transpose((1,2,0))
 
 
+e_build_sipm_waveforms = jit(vmap(build_sipm_waveforms, in_axes=[0,0,0,0,None,]))
+batch_build_sipm_waveforms = jit(vmap(e_build_sipm_waveforms, in_axes=[0,0,0,0,None,]))

src/trainers/supervised_trainer.py
@@ -27,11 +27,11 @@ def compute_loss(simulated_waveforms, real_waveforms):
     difference = (simulated_waveforms - real_waveforms)**2.
     # Because this data is so sparse, we multiply the squared difference
     # by the input data to push the loss up in important places and
-    # down in unimportant places.  But, don't want the loss to be zero 
+    # down in unimportant places.  But, don't want the loss to be zero
     # where the wavefunction is zero, so put a floor:
     loss = difference
     # loss = difference * (real_waveforms + 1e-4)
-    
+
     # And, return the loss as a scalar:
     return loss.mean()
 
@@ -40,11 +40,11 @@ def compute_loss(simulated_waveforms, real_waveforms):
 
 @jit
 def apply_gradients(_parameters, _gradients, learning_rate):
-    
+
     # Flatten the tree:
     p_flat_tree, p_tree_def = tree_util.tree_flatten(_parameters)
     g_flat_tree, g_tree_def = tree_util.tree_flatten(_gradients)
-    
+
     new_params = [p - learning_rate * g for p, g in zip(p_flat_tree, g_flat_tree)]
 
     return tree_util.tree_unflatten(p_tree_def, new_params)
@@ -73,18 +73,23 @@ class supervised_trainer:
 
         @jit
         def forward_pass(batch, _parameters, key):
-            simulated_pmts = simulate_fn(batch['energy_deposits'], _parameters, key)
-            loss = compute_loss(simulated_pmts, batch['S2Pmt'])
-            return loss, simulated_pmts
+            simulated_pmts, simulated_sipms = simulate_fn(batch['energy_deposits'], _parameters, key)
+            loss_pmt = compute_loss(simulated_pmts, batch['S2Pmt'])
+            loss_sipm = compute_loss(simulated_sipms, batch['S2Si'])
+            loss = loss_pmt + loss_sipm
+            # print(loss)
+            return loss
+            # return loss, ((loss_pmt, loss_sipm), (simulated_pmts, simulated_sipms))
 
-        self.gradient_fn = jit(jax.value_and_grad(forward_pass, argnums=1, has_aux=True))
+        # self.gradient_fn = jit(jax.value_and_grad(forward_pass, argnums=1, has_aux=True))
+        self.gradient_fn = jit(jax.value_and_grad(forward_pass, argnums=1, has_aux=False))
 
 
 
 
 
-        opt_init, opt_update, get_params = jax_opt.adam(1e-3)
-        
+        opt_init, opt_update, get_params = jax_opt.adam(1e-2)
+
         # Initialize the optimizer:
         self.opt_state = opt_init(parameters)
 
@@ -124,88 +129,96 @@ class supervised_trainer:
         return
 
 
-    # def plot_sipms(self, plot_dir, sim_sipms, real_sipms):
+    def plot_sipms(self, plot_dir, sim_sipms, real_sipms):
 
-    #     x_ticks = numpy.arange(550)
-    #     plot_dir.mkdir(parents=True, exist_ok=True)
+        x_ticks = numpy.arange(550)
+        plot_dir.mkdir(parents=True, exist_ok=True)
 
-    #     # Find the index of the peak sipm location:
-    #     max_value = tf.reduce_max(real_sipms)
-    #     max_loc   = tf.where(real_sipms == max_value)[0]
-
-    #     # Draw the response sliced across the three dimensions:
-    #     # Slice on z:    
-
-    #     # This plots over all z, around the highest value sipm:
-    #     central_i_x = max_loc[0]
-    #     central_i_y = max_loc[1]
-    #     central_i_z = max_loc[2]
-
-    #     for i_x in [central_i_x -1, central_i_x, central_i_x + 1]:
-    #         if i_x < 0 or i_x >= 47: continue
-    #         for i_y in [central_i_y -1, central_i_y, central_i_y + 1]:
-    #             if i_y < 0 or i_y >= 47: continue
-
-    #             fig = plt.figure(figsize=(16,9))
-    #             plt.plot(x_ticks, sim_sipms[i_x][i_y], label=f"Generated SiPM [{i_x}, {i_y}] signal")
-    #             plt.plot(x_ticks, real_sipms[i_x][i_y], label=f"Real SiPM [{i_x}, {i_y}] signal")
-    #             plt.legend()
-    #             plt.grid(True)
-    #             plt.xlabel("Time Tick [us]")
-    #             plt.ylabel("Amplitude")
-    #             plt.savefig(plot_dir / pathlib.Path(f"sipm_{i_x}_{i_y}.png"))
-    #             plt.tight_layout()
-    #             plt.close()
-
-    #     # This plots x and y for a fixed z:
-    #     for i_z in [central_i_z -1, central_i_z, central_i_z + 1]:
-    #         if i_z < 0 or i_z >= 550: continue
-
-    #         # 
-    #         fig = plt.figure()
-    #         plt.imshow(sim_sipms[:,:,i_z])
-    #         plt.tight_layout()
-    #         plt.savefig(plot_dir / pathlib.Path(f"sim_sipm_slice_{i_z}.png"))
-    #         plt.close()
-
-    #         fig = plt.figure()
-    #         plt.imshow(real_sipms[:,:,i_z])
-    #         plt.tight_layout()
-    #         plt.savefig(plot_dir / pathlib.Path(f"real_sipm_slice_{i_z}.png"))
-    #         plt.close()
+        print(real_sipms.shape)
 
 
-    # def plot_compressed_sipms(self, plot_dir, sim_sipms, real_sipms, axis):
-        
-    #     plot_dir.mkdir(parents=True, exist_ok=True)
-        
-    #     # What is the axis label for this compression?
-    #     if axis == 0:
-    #         label = "x"
-    #     elif axis == 1:
-    #         label = "y"
-    #     elif axis == 2:
-    #         label = "z"
-    #     else:
-    #         raise Exception(f"Invalid axis {axis} provided to compression plots.")
+        # Find the index of the peak sipm location:
+        max_value = numpy.max(real_sipms)
+        max_x, max_y, max_z = numpy.unravel_index(numpy.argmax(real_sipms), real_sipms.shape)
 
-    #     # Compress time ticks:
-    #     sim_comp = tf.reduce_sum(sim_sipms, axis=axis)
+        # This plots over all z, around the highest value sipm:
+        for i_x in [max_x -1, max_x, max_x + 1]:
+            if i_x < 0 or i_x >= 47: continue
+            for i_y in [max_y -1, max_y, max_y + 1]:
+                if i_y < 0 or i_y >= 47: continue
 
-    #     fig = plt.figure()
-    #     plt.imshow(sim_comp)
-    #     plt.tight_layout()
-    #     plt.savefig(plot_dir / pathlib.Path(f"sim_sipm_compress_{label}.png"))
-    #     plt.close()
+                fig = plt.figure(figsize=(16,9))
+                plt.plot(x_ticks, sim_sipms[i_x][i_y], label=f"Generated SiPM [{i_x}, {i_y}] signal")
+                plt.plot(x_ticks, real_sipms[i_x][i_y], label=f"Real SiPM [{i_x}, {i_y}] signal")
+                plt.legend()
+                plt.grid(True)
+                plt.xlabel("Time Tick [us]")
+                plt.ylabel("Amplitude")
+                plt.savefig(plot_dir / pathlib.Path(f"sipm_{i_x}_{i_y}.png"))
+                plt.tight_layout()
+                plt.close()
 
-    #     # Compress time ticks:
-    #     real_comp = tf.reduce_sum(real_sipms, axis=axis)
+        # This plots x and y for a fixed z:
+        for i_z in [max_z -1, max_z, max_z + 1]:
+            if i_z < 0 or i_z >= 550: continue
 
-    #     fig = plt.figure()
-    #     plt.imshow(real_comp)
-    #     plt.tight_layout()
-    #     plt.savefig(plot_dir / pathlib.Path(f"real_sipm_compress_{label}.png"))
-    #     plt.close()
+            #
+            fig = plt.figure()
+            plt.imshow(sim_sipms[:,:,i_z])
+            plt.tight_layout()
+            plt.savefig(plot_dir / pathlib.Path(f"sim_sipm_slice_{i_z}.png"))
+            plt.close()
+
+            fig = plt.figure()
+            plt.imshow(real_sipms[:,:,i_z])
+            plt.tight_layout()
+            plt.savefig(plot_dir / pathlib.Path(f"real_sipm_slice_{i_z}.png"))
+            plt.close()
+
+
+    def plot_compressed_sipms(self, plot_dir, sim_sipms, real_sipms, axis):
+
+        plot_dir.mkdir(parents=True, exist_ok=True)
+
+        # What is the axis label for this compression?
+        if axis == 0:
+            label = "x"
+        elif axis == 1:
+            label = "y"
+        elif axis == 2:
+            label = "z"
+        else:
+            raise Exception(f"Invalid axis {axis} provided to compression plots.")
+
+        # Compress time ticks:
+        sim_comp = numpy.sum(sim_sipms, axis=axis)
+
+        fig = plt.figure()
+        plt.imshow(sim_comp)
+        plt.tight_layout()
+        plt.savefig(plot_dir / pathlib.Path(f"sim_sipm_compress_{label}.png"))
+        plt.close()
+
+        # Compress time ticks:
+        real_comp = numpy.sum(real_sipms, axis=axis)
+
+        fig = plt.figure()
+        plt.imshow(real_comp)
+        plt.tight_layout()
+        plt.savefig(plot_dir / pathlib.Path(f"real_sipm_compress_{label}.png"))
+        plt.close()
+
+    def parameters(self):
+
+        p = self.get_params(self.opt_state)
+        # Deliberately slice things up here:
+
+        parameters = {}
+        parameters["diffusion/x"] = p["diffusion"][0]
+        parameters["diffusion/y"] = p["diffusion"][1]
+        parameters["diffusion/z"] = p["diffusion"][2]
+        parameters["lifetime"] = p["lifetime"]
+        return parameters
 
     def comparison_plots(self, plot_directory):
 
@@ -219,10 +232,9 @@ class supervised_trainer:
 
         self.key, subkey = jax.random.split(self.key)
         # First, run the monitor data through the simulator:
-        simulated_pmts = self.simulate_fn(self.monitor_data['energy_deposits'], parameters, subkey)
+        simulated_pmts, simulated_sipms = self.simulate_fn(self.monitor_data['energy_deposits'], parameters, subkey)
 
         # Save the raw data into a file:
-        print(plot_directory)
         plot_directory.mkdir(parents=True, exist_ok=True)
         # numpy.savez_compressed(plot_directory / pathlib.Path(f"output_arrays.npz"),
         #     real_pmts  = self.monitor_data["S2Pmt"],
@@ -241,27 +253,23 @@ class supervised_trainer:
         pmt_dir = plot_directory / pathlib.Path(f"pmts/")
         self.plot_pmts(pmt_dir, simulated_pmts[batch_index], self.monitor_data["S2Pmt"][batch_index])
 
-        # sim_data_3d  = gen_s2_si[batch_index]
-        # real_data_3d = self.monitor_data["S2Si"][batch_index]
+        sim_data_3d  = simulated_sipms[batch_index]
+        real_data_3d = self.monitor_data["S2Si"][batch_index]
 
 
-        # sipm_dir = plot_directory / pathlib.Path(f"sipms/")
-        # self.plot_sipms(sipm_dir, sim_data_3d, real_data_3d)
+        sipm_dir = plot_directory / pathlib.Path(f"sipms/")
+        self.plot_sipms(sipm_dir, sim_data_3d, real_data_3d)
 
 
-        # # Take 2D compression views:
-        # self.plot_compressed_sipms(sipm_dir, sim_data_3d, real_data_3d, axis=0)
-        # self.plot_compressed_sipms(sipm_dir, sim_data_3d, real_data_3d, axis=1)
-        # self.plot_compressed_sipms(sipm_dir, sim_data_3d, real_data_3d, axis=2)
+        # Take 2D compression views:
+        self.plot_compressed_sipms(sipm_dir, sim_data_3d, real_data_3d, axis=0)
+        self.plot_compressed_sipms(sipm_dir, sim_data_3d, real_data_3d, axis=1)
+        self.plot_compressed_sipms(sipm_dir, sim_data_3d, real_data_3d, axis=2)
 
 
 
 
     def train_iteration(self, batch, i):
-
-        # Max Lox:
-
-        i_max = batch["S2Pmt"][0][0].argmax()
 
         metrics = {}
 
@@ -269,19 +277,12 @@ class supervised_trainer:
 
         self.key, subkey = jax.random.split(self.key)
 
-        (loss, waveforms), gradients = self.gradient_fn(batch, parameters, subkey)
+
+        # (loss, (loss_pmts, loss_sipms), (pmts, sipms) ), gradients = self.gradient_fn(batch, parameters, subkey)
+        loss, gradients = self.gradient_fn(batch, parameters, subkey)
 
         self.opt_state = self.opt_update(i, gradients, self.opt_state)
 
-        print(batch["S2Pmt"][0][0][i_max - 5: i_max + 5])
-        print(waveforms[0][0][i_max - 5: i_max + 5])
-
-
-        logger.info(waveforms.max())
-        logger.info(parameters["lifetime"])
-        logger.info(parameters["diffusion"])
-        logger.info(parameters["pmt_dynamic_range"])
-        logger.info(parameters["waveform_sigma"])
 
 
         # self.parameters = apply_gradients(self.parameters, gradients, learning_rate=0.01)